5d37a87a5ca296d923817db7bdcee2d4ac4038a3
MonkEMon221 <vishesh9778@gmail.com>
2023-06-22 14:20:09 +0530

    First Commit

---
+++ components/Nav.jsx
@@ -0,0 +1,140 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, getProviders, useSession } from "next-auth/react";
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex-gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text ml-2">Promptopia</p>
+      </Link>
+
+      {/* Desktop Nav */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button
+              type="button"
+              onClick={() => signOut()}
+              className="outline_btn"
+            >
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                alt="Profile"
+                className="rounded-full"
+              ></Image>
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Nav */}
+      <div className="flex relative sm:hidden">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              alt="Profile"
+              className="rounded-full cursor-pointer"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            ></Image>
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
